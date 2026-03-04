26358eb06ea0e44b1b46fdeeb5a50dd1e13ba24b
Daniel Lewis <github@mrdaniellewis.co.uk>
2026-03-04 12:41:57 +0000

    add yankstack

--- init.lua
+++ init.lua
@@ -111,6 +111,8 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 
+vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+
 vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { underline = true })
 
 vim.lsp.config('*', {
